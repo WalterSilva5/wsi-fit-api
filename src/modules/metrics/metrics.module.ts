9f185793c64c6db0df11f3555dade9729f1b7862
Walter Pereira Da Silva <waltersilva5@outlook.com>
2024-11-17 15:22:20 -0300

    remove invalid modules

--- src/modules/metrics/metrics.module.ts
+++ src/modules/metrics/metrics.module.ts
@@ -2,16 +2,12 @@ import { PrismaModule } from '../../database/prisma/prisma.module';
 import { MetricsController } from './metrics.controller';
 import { MetricsService } from './metrics.service';
 import { Module } from '@nestjs/common';
-import { EventsGateway } from 'src/modules/gateway/events.gateway';
-import { ApikeyService } from 'src/modules/apikey/apikey.service';
-import { ApikeyRepository } from 'src/modules/apikey/apikey.repository';
 import { UserModule } from 'src/modules/user/user.module';
-import { GatewayModule } from '../gateway/gateway.module';
 import { UserActivityRegistry } from 'src/modules/user/user.registry';
 @Module({
-  providers: [MetricsService, EventsGateway, ApikeyRepository, ApikeyService],
+  providers: [MetricsService],
   controllers: [MetricsController],
   exports: [MetricsService],
-  imports: [PrismaModule, UserModule, UserActivityRegistry, GatewayModule]
+  imports: [PrismaModule, UserModule, UserActivityRegistry]
 })
 export class MetricsModule {}
--- src/modules/metrics/metrics.service.ts
+++ src/modules/metrics/metrics.service.ts
@@ -1,29 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { EventsGateway } from 'src/modules/gateway/events.gateway';
 import { UserActivityRegistry } from 'src/modules/user/user.registry';
 @Injectable()
 export class MetricsService {
   constructor(
-    protected readonly eventsGateway: EventsGateway,
     protected readonly userActivityRegistry: UserActivityRegistry
   ) {}
 
   async getMetrics(): Promise<string> {
     let metricsOutput = '';
-    metricsOutput += this.getMetricsForActiveSessions();
     metricsOutput += this.getMetricsForActiveUsers();
     return metricsOutput;
-  }
-
-  private getMetricsForActiveSessions(): string {
-    const sessionsCount = this.eventsGateway.activeSessions;
-    return (
-      [
-        '# HELP active_sessions Número de sessões ativas.',
-        '# TYPE active_sessions gauge',
-        `active_sessions ${sessionsCount}`
-      ].join('\n') + '\n'
-    );
   }
 
   private getMetricsForActiveUsers(): string {
